7b9ab43feb0dcd30f29c01c7df0227f1c7b4cf53
David Clark <david@kit.fit>
2019-03-05 14:45:13 +1100

    working with default parcel

--- libSrc/something.js
+++ libSrc/something.js
@@ -1,3 +1,4 @@
+import a from './a'
 // funky stuff happens with the default export
 
 // this should be remain fat arrow function for main build, but be a function def for lib
--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import { performance } from 'perf_hooks'
 import something from '../libSrc/something'
 
 const foo = (n) => n**2
-
-const a = performance.now()
 
 var arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 for (var i = 0; i < arr.length; i++) {
 	console.log(something(foo(arr[i])))
 }
 
-const b = performance.now()
-
 for (let x of [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) {
 	console.log(something(foo(x)))
 }
-
-const c = performance.now()
-
-console.log('performance delta, smaller is better')
-console.log('for loop', b-a)
-console.log('for of', c-b)
